ff1474e64e6b77c5e7eb47f2c7542e9a75811210
Michael Wiencek <mwtuea@gmail.com>
2018-03-08 13:56:38 -0600

    Add large reduce chain to benchmark

--- bench.js
+++ bench.js
@@ -2,9 +2,11 @@ require('babel-register');
 
 const Benchmark = require('benchmark');
 const _compact = require('lodash/fp/compact');
+const _compose = require('lodash/fp/compose');
 const _flattenDeep = require('lodash/fp/flattenDeep');
 const _flowRight = require('lodash/fp/flowRight');
 const _map = require('lodash/fp/map');
+const _reduce = require('lodash/fp/reduce');
 const _uniq = require('lodash/fp/uniq');
 const R = require('ramda');
 const it = require('./src/index');
@@ -50,6 +52,52 @@ const getNewIdsRamda = R.compose(
   })
   .add('ramda', function () {
     getNewIdsRamda(mediums);
+  })
+  .on('cycle', function (event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .on('error', function (err) {
+    console.error(err);
+  })
+  .run({async: false});
+
+const largeList = R.repeat(1, 1000);
+const sum = (accum, x) => accum + x;
+const double = x => x * 2;
+const doubleIt = it.map(double);
+const doubleLodash = _map(double);
+const doubleRamda = R.map(double);
+
+(new Benchmark.Suite)
+  .add('terable', function () {
+    it.reduce(sum, 0)(it.compose(
+      doubleIt,
+      doubleIt,
+      doubleIt,
+      doubleIt,
+      doubleIt,
+    )(largeList));
+  })
+  .add('lodash/fp', function () {
+    _reduce(sum, 0)(_compose(
+      doubleLodash,
+      doubleLodash,
+      doubleLodash,
+      doubleLodash,
+      doubleLodash,
+    )(largeList));
+  })
+  .add('ramda', function () {
+    R.reduce(sum, 0)(R.compose(
+      doubleRamda,
+      doubleRamda,
+      doubleRamda,
+      doubleRamda,
+      doubleRamda,
+    )(largeList));
   })
   .on('cycle', function (event) {
     console.log(String(event.target));
