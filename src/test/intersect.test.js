ef5ac3b96d612e403774ece38fe2ecddaa3af956
Michael Wiencek <mwtuea@gmail.com>
2018-03-15 18:44:03 -0500

    More intersect tests

--- src/test/intersect.test.js
+++ src/test/intersect.test.js
@@ -15,6 +15,19 @@ test('intersect', () => {
   const set = intersect([source, target]);
   expect(toArray(set)).toEqual([2, 4, 6]);
 
+  const a = Symbol();
+  const b = Symbol();
+  const c = Symbol();
+  const d = Symbol();
+  expect(toArray(intersect([[a, b, c, d], [b, c], [c, d]]))).toEqual([c]);
+
+  // Set intersection
+  expect(toArray(intersect([
+    new Set([a, b, c, d]),
+    new Set([b, c]),
+    new Set([c, d]),
+  ]))).toEqual([c]);
+
   expect(toArray(intersect(
     map(x => [x - 1, x, x + 1])([1, 2, 3])
   ))).toEqual([2]);
