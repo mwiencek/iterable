768968ba2ffbccd45507f412f876c726c05d7443
Michael Wiencek <mwtuea@gmail.com>
2018-10-16 09:19:44 -0500

    async any

---
+++ src/async/any.js
@@ -0,0 +1,13 @@
+/*
+ * @flow
+ * Copyright (c) 2018 Michael Wiencek
+ *
+ * This source code is licensed under the MIT license. A copy can be found
+ * in the file named "LICENSE" at the root directory of this distribution.
+ */
+
+import boolFold from './util/boolFold';
+
+const any = boolFold(true);
+
+export default any;
--- src/test/any.test.js
+++ src/test/any.test.js
@@ -6,15 +6,25 @@ import {
   map,
   take,
 } from '../';
-import {closeable, throws} from './util';
+import asyncAny from '../async/any';
+import asyncMap from '../async/map';
+import {closeable, throws, makeAsyncIterator} from './util';
 
-test('any', () => {
+test('any', async function () {
   const array = [1, 2, 3];
 
   expect(any(x => x > 2)([])).toBe(false);
+  expect(await asyncAny(x => x > 2)(makeAsyncIterator([]))).toBe(false);
+
   expect(any(x => x > 2)(array)).toBe(true);
+  expect(await asyncAny(x => x > 2)(makeAsyncIterator(array))).toBe(true);
+
   expect(any(x => x < 1)(array)).toBe(false);
+  expect(await asyncAny(x => x < 1)(makeAsyncIterator(array))).toBe(false);
+
   expect(any(x => x > 0)(map(x => -x)(array))).toBe(false);
+  expect(await asyncAny(x => x > 0)(asyncMap(x => -x)(makeAsyncIterator(array)))).toBe(false);
+
   expect(any(x => x === 0)(drop(1)(map(x => x - 1)(array)))).toBe(false);
   expect(any(x => x === 0)(take(1)(map(x => x - 1)(array)))).toBe(true);
 });
