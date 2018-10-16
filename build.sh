6701c2370fc1e66fd8e4379f4ec5cfdccaf41590
Michael Wiencek <mwtuea@gmail.com>
2018-10-16 12:18:08 -0500

    Fix babel commands
    These are affected by the cwd

--- build.sh
+++ build.sh
@@ -8,12 +8,10 @@ yarn install
 
 mkdir -p dist/{async/util,util}
 
-pushd src
-../node_modules/.bin/babel *.js --out-dir ../dist/
-../node_modules/.bin/babel async/*.js --out-dir ../dist/async/
-../node_modules/.bin/babel async/util/*.js --out-dir ../dist/async/util/
-../node_modules/.bin/babel util/*.js --out-dir ../dist/util/
-popd
+./node_modules/.bin/babel src/*.js --out-dir dist/
+./node_modules/.bin/babel src/async/*.js --out-dir dist/async/
+./node_modules/.bin/babel src/async/util/*.js --out-dir dist/async/util/
+./node_modules/.bin/babel src/util/*.js --out-dir dist/util/
 
 cp \
     .npmignore \
