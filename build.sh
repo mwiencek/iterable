35ce918752b36b346fb5f231c135477babcd2fc3
Michael Wiencek <mwtuea@gmail.com>
2018-10-16 09:58:22 -0500

    Copy async/util to dist

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 yarn install
 
-mkdir -p dist/{async,util}
+mkdir -p dist/{async/util,util}
 
 pushd src
 ../node_modules/.bin/babel *.js --out-dir ../dist/
 ../node_modules/.bin/babel async/*.js --out-dir ../dist/async/
+../node_modules/.bin/babel async/util/*.js --out-dir ../dist/async/util/
 ../node_modules/.bin/babel util/*.js --out-dir ../dist/util/
 popd
 
